3e86179b773262c95f8fdc8632838de871ff62ce
Ayysir <ayysir@aospal.com>
2015-08-05 18:13:31 -0400

    OnTheSpot: Allow removal of immersive messages
    Change-Id: I1a0361578a6aaa8e3e5f8a8fcc1d9a004112dcdf

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3279,6 +3279,12 @@ public final class Settings {
          */
         public static final String QUICK_SETTINGS_QUICK_PULL_DOWN = "quick_settings_quick_pull_down";
 
+       /**
+        * Disable Immersive Message
+        * @hide
+        */
+        public static final String DISABLE_IMMERSIVE_MESSAGE = "disable_immersive_message";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
@@ -3387,7 +3393,8 @@ public final class Settings {
          * @hide
          */
         public static final String[] SETTINGS_TO_RESET = {
-            QUICK_SETTINGS_QUICK_PULL_DOWN
+            QUICK_SETTINGS_QUICK_PULL_DOWN,
+            DISABLE_IMMERSIVE_MESSAGE
         };
 
         // Settings moved to Settings.Secure
