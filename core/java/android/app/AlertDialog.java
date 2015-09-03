TeamBliss-LP/android_frameworks_base

Dark Material themed Advanced Power Menu
Change-Id: I672203f0da85a97870e6272d6b8a2f73758c6ec3

core/java/android/app/AlertDialog.java
@@ -93,6 +93,14 @@ public class AlertDialog extends Dialog implements DialogInterface {
      */
     public static final int THEME_DEVICE_DEFAULT_LIGHT = 5;
 
+
+    /**
+     * Special theme constant for {@link #AlertDialog(Context, int)}: use
+     * the material alert theme with a dark background.
+     */
+    public static final int THEME_MATERIAL = 6;
+
+
     /**
      * No layout hint.
      * @hide
@@ -147,6 +155,8 @@ public class AlertDialog extends Dialog implements DialogInterface {
             return com.android.internal.R.style.Theme_DeviceDefault_Dialog_Alert;
         } else if (resid == THEME_DEVICE_DEFAULT_LIGHT) {
             return com.android.internal.R.style.Theme_DeviceDefault_Light_Dialog_Alert;
+        } else if (resid == THEME_MATERIAL) {
+            return com.android.internal.R.style.Theme_Material_BlissDark_Dialog_Alert;
         } else if (resid >= 0x01000000) {   // start of real resource IDs.
             return resid;
         } else {

chickenfactory/android_frameworks_base-3

Conflicts:
	core/res/res/values/cr_symbols.xml
	services/core/java/com/android/server/power/ShutdownThread.java

core/java/android/app/AlertDialog.java
@@ -93,6 +93,14 @@ public class AlertDialog extends Dialog implements DialogInterface {
      */
     public static final int THEME_DEVICE_DEFAULT_LIGHT = 5;
 
+
+    /**
+     * Special theme constant for {@link #AlertDialog(Context, int)}: use
+     * the material alert theme with a dark background.
+     */
+    public static final int THEME_MATERIAL = 6;
+
+
     /**
      * No layout hint.
      * @hide
@@ -147,6 +155,8 @@ public class AlertDialog extends Dialog implements DialogInterface {
             return com.android.internal.R.style.Theme_DeviceDefault_Dialog_Alert;
         } else if (resid == THEME_DEVICE_DEFAULT_LIGHT) {
             return com.android.internal.R.style.Theme_DeviceDefault_Light_Dialog_Alert;
+        } else if (resid == THEME_MATERIAL) {
+            return com.android.internal.R.style.Theme_Material_Trds_Dialog_Alert;
         } else if (resid >= 0x01000000) {   // start of real resource IDs.
             return resid;
         } else {